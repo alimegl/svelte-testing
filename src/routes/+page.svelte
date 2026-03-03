1f4faa32effdca5c910b24386dccfac1e0f9f2f2
alimegl <alim.erguel@volkswagen.de>
2026-03-03 11:25:19 +0100

    fix: update language attribute in app.html and enhance +page.svelte with interactive elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    let clickCount = $state(0);
+</script>
+
+
+<h1>Svelte testing repo :)</h1>
+<p>Hier geht es zu meinem GitHub: <a href="https://github.com/alimegl">github.com/alimegl</a></p>
+<p>Here i'm trying out svelte!</p>
+
+
+
+<button onclick={() => clickCount++} title="Das ist ein Button!">
+    clickCount: {clickCount}
+</button>
+
+<br>
+
+<button onclick={() => clickCount = 0} title="Das ist ein Button, welcher den Counter resettet!">
+    Reset clickCount
+</button>
+
+
+<style>
+	h1 {
+		color: red;
+        text-align: center;
+        font-size: 3em;
+	}
+
+    p {
+        color: blue;
+        text-align: center;
+        font-size: 1.5em;
+    }
+    button {
+        display: block;
+        margin: 0 auto;
+    }
+</style>
